8456322312bacdc5e1f84ef7cbc65bf9c14f6b8a
Arnav <mail2arnav30@gmail.com>
2025-08-09 13:02:05 +0530

    Uploaded Prework files

---
+++ arnav/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  arnav
+//
+//  Created by Arnav Sharma on 09/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addBackground(_ sender: UIButton) {
+        let randomColor = changeColor()
+           view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+
+}
+
---
+++ arnavTests/arnavTests.swift
@@ -0,0 +1,17 @@
+//
+//  arnavTests.swift
+//  arnavTests
+//
+//  Created by Arnav Sharma on 09/08/25.
+//
+
+import Testing
+@testable import arnav
+
+struct arnavTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
